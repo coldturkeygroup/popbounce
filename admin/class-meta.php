6363f5e2327746214a7c1f6531da029c77fdcaa2
Aaron Huisinga <aaron@coldturkeygroup.com>
2015-04-03 15:26:17 -0500

    Modify existing plugin for CTG
    1. Rename to popBounce and change references in admin panel to Exit
    Intent Ads to help with familiarity.
    2. Allow for full page ads instead of simple popover modals.
    3. Remove several confusing and unnecessary options.
    4. Simplify the design of the options page.

--- admin/class-meta.php
+++ admin/class-meta.php
@@ -1,172 +1,168 @@
 <?php
+
 /**
- * @package Admin
+ * Class Popbounce_Meta
+ *
+ * Class for rendering popBounce meta boxes
  */
-class Wbounce_Meta {
+class Popbounce_Meta {
 
-	private $select_name = 'wbounce_status';
+	private $select_name = 'popbounce_status';
 
-	function __construct() {
+	/**
+	 * Constructor for the meta class
+	 */
+	public function __construct()
+	{
 		$this->init_meta_boxes();
 		$this->init_post_columns();
 	}
 
 	/**
-	 * Add additonal fields to the page where you create your posts and pages
-	 * (Based on http://wp.tutsplus.com/tutorials/plugins/how-to-create-custom-wordpress-writemeta-boxes/)
+	 * Initialize meta boxes on posts & pages editor
 	 */
-	function init_meta_boxes() {
-		add_action( 'add_meta_boxes', array( $this, 'add_meta_box' ) );
-		add_action( 'save_post', array( $this, 'save' ) );
+	public function init_meta_boxes()
+	{
+		add_action( 'add_meta_boxes', [ $this, 'add_meta_box' ] );
+		add_action( 'save_post', [ $this, 'save' ] );
 	}
 
-	function add_meta_box() {
-
-		$screens = array( 'post', 'page' );
+	/**
+	 * Define where the meta box should appear & render
+	 */
+	public function add_meta_box()
+	{
+		$screens = [ 'post', 'page' ];
 
 		foreach ( $screens as $screen ) {
 			add_meta_box(
-				'meta-box-wbounce',
-				'wBounce by Kevin Weber',
-				array( $this, 'meta_box' ),
+				'meta-box-popbounce',
+				'Exit Intent Ad',
+				[ $this, 'meta_box' ],
 				$screen,
-				'side',	// position
-				'high'	// priority
+				'side',
+				'low'
 			);
 		}
-
 	}
 
-	function meta_box( $post ) {
-		$values = get_post_custom( $post->ID );
-		// $text = isset( $values['oembed_link'] ) ? esc_attr( $values['oembed_link'][0] ) : '';
-		// $check = isset( $values['wbounce_check_custom'] ) ? esc_attr( $values['wbounce_check_custom'][0] ) : '';
-		
-		$select_name = $this->select_name;
-			$selected = isset( $values[$select_name] ) ? esc_attr( $values[$select_name][0] ) : '';
-
-		wp_nonce_field( 'my_meta_box_nonce', 'meta_box_nonce' );
-
+	/**
+	 * Build the meta box content
+	 *
+	 * @param $post
+	 */
+	public function meta_box( $post )
+	{
+		$select_name        = $this->select_name;
+		$meta_element_class = get_post_meta( $post->ID, $select_name, true );
+		wp_nonce_field( 'popbounce_nonce', 'popbounce_nonce' );
 		?>
 
-<!-- 		<h4>Custom Text</h4>
+		<p style="margin-top:0"><label for="<?= $select_name; ?>">Show exit intent ad on
+				this <?= get_current_screen()->post_type; ?>?</label></p>
 		<p>
-			<p>Description &hellip;</p>
-			<input type="text" name="oembed_link" id="oembed_link" value="<?php //echo $text; ?>" style="width:100%;" />
-			<label for="oembed_link">E.g. <i>example</i></label>	
-		</p> -->
-
-<!-- 		<h4>Checkbox</h4>
-		<p>
-			<input type="checkbox" name="wbounce_check_custom" id="wbounce_check_custom" <?php // checked( $check, 'on' ); ?> />
-			<label for="wbounce_check_custom">If checked: Display ...</label>
-		</p> -->
-
-		<h4>Status</h4>
-		<p><label for="<?php echo $select_name; ?>">Use wBounce on this <?php echo get_current_screen()->post_type; ?>?</label></p>
-		<p>
-			<select class="select" type="select" name="<?php echo $select_name; ?>" id="<?php echo $select_name; ?>">
-			<?php $meta_element_class = get_post_meta($post->ID, $select_name, true);	?>
-		      <option value="default" <?php selected( $meta_element_class, 'default' ); ?>>Default</option>
-		      <option value="on" <?php selected( $meta_element_class, 'on' ); ?>>On</option>
-		      <option value="off" <?php selected( $meta_element_class, 'off' ); ?>>Off</option>
+			<select class="select" name="<?= $select_name; ?>" id="<?= $select_name; ?>">
+				<option value="default" <?php selected( $meta_element_class, 'default' ); ?>>Default</option>
+				<option value="on" <?php selected( $meta_element_class, 'on' ); ?>>On</option>
+				<option value="off" <?php selected( $meta_element_class, 'off' ); ?>>Off</option>
 			</select>
 		</p>
-
 	<?php }
 
-	function save( $post_id ) {
+	/**
+	 * Save the data from the meta box
+	 *
+	 * @param $post_id
+	 */
+	public function save( $post_id )
+	{
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return;
 
-		// Bail if we're doing an auto save
-		if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
-		
-		// If our nonce isn't there, or we can't verify it, bail
-		if( !isset( $_POST['meta_box_nonce'] ) || !wp_verify_nonce( $_POST['meta_box_nonce'], 'my_meta_box_nonce' ) ) return;
-		
-		// Now we can actually save the data
-		$allowed = array( 
-			'a' => array( // on allow a tags
-				'href' => array() // and those anchords can only have href attribute
-			)
-		);
-		
-		// Probably a good idea to make sure your data is set
+		// nonce validation
+		if ( ! isset( $_POST['popbounce_nonce'] ) || ! wp_verify_nonce( $_POST['popbounce_nonce'], 'popbounce_nonce' ) )
+			return;
 
-		// TEXTBOX
-		// if( isset( $_POST['oembed_link'] ) )
-		// 	update_post_meta( $post_id, 'oembed_link', wp_kses( $_POST['oembed_link'], $allowed ) );
-
-		// CHECKBOX
-		// $chk = ( isset( $_POST['wbounce_check_custom'] ) && $_POST['wbounce_check_custom'] ) ? 'on' : 'off';
-		// update_post_meta( $post_id, 'wbounce_check_custom', $chk );
-
-		// SELECT
 		$select_name = $this->select_name;
-		if( isset( $_POST[$select_name] ) )
-			update_post_meta( $post_id, $select_name, esc_attr( $_POST[$select_name] ) );
-		
+		if ( isset( $_POST[ $select_name ] ) )
+			update_post_meta( $post_id, $select_name, esc_attr( $_POST[ $select_name ] ) );
 	}
 
 
-
 	/**
-	 * Add custom column
-	 * (Based on http://shibashake.com/wordpress-theme/expand-the-wordpress-quick-edit-menu)
+	 * Add custom column for posts and pages tables
 	 */
-	function init_post_columns() {
-
-		$screens = array( 'posts', 'pages' );
+	function init_post_columns()
+	{
+		$screens = [ 'posts', 'pages' ];
 
 		foreach ( $screens as $screen ) {
-			add_filter( 'manage_'.$screen.'_columns', array( $this, 'add_post_columns' ) );
-			add_action( 'manage_'.$screen.'_custom_column', array( $this, 'render_post_columns' ), 10, 2 );
+			add_filter( 'manage_' . $screen . '_columns', [ $this, 'add_post_columns' ] );
+			add_action( 'manage_' . $screen . '_custom_column', [ $this, 'render_post_columns' ], 10, 2 );
 		}
 
-		add_action( 'admin_footer', array( $this, 'post_columns_css' ) );
-	}
-	 
-	function add_post_columns($columns) {
-	    $columns['wbounce'] = 'wBounce';
-	    return $columns;
-	}
-
-	function render_post_columns($column_name, $id) {
-		$select_name = $this->select_name;
-		$wbounce_status = 'default';
-		$wbounce_title = 'Default';
-
-	    switch ($column_name) {
-	    case 'wbounce':
-	        $widget_id = get_post_meta( $id, $select_name, true );
-
-	        if ($widget_id) {
-	        	$get_post_custom = get_post_meta( $id, $select_name, true );
-		        switch ($get_post_custom) {
-		        	case 'on' :
-		        		$wbounce_status = 'on';
-		        		$wbounce_title = 'On';
-		        		break;
-		        	case 'off' :
-		        		$wbounce_status = 'off';
-		        		$wbounce_title = 'Off';
-		        		break;
-		        }
-	        }
-	    	echo '<div title="'. $wbounce_title .'" class="wbounce-status '. $wbounce_status .'"></div>';
-	    }
-
+		add_action( 'admin_footer', [ $this, 'post_columns_css' ] );
 	}
 
 	/**
-	 * Add CSS for post columns
+	 * Add the defined custom column
+	 *
+	 * @param $columns
+	 *
+	 * @return mixed
 	 */
-	function post_columns_css() {
+	function add_post_columns( $columns )
+	{
+		$columns['popbounce'] = 'popBounce';
+
+		return $columns;
+	}
+
+	/**
+	 * Render our custom column
+	 *
+	 * @param $column_name
+	 * @param $id
+	 */
+	function render_post_columns( $column_name, $id )
+	{
+		$select_name      = $this->select_name;
+		$popbounce_status = 'default';
+		$popbounce_title  = 'Default';
+
+		switch ( $column_name ) {
+			case 'popbounce':
+				$widget_id = get_post_meta( $id, $select_name, true );
+
+				if ( $widget_id ) {
+					$get_post_custom = get_post_meta( $id, $select_name, true );
+					switch ( $get_post_custom ) {
+						case 'on' :
+							$popbounce_status = 'on';
+							$popbounce_title  = 'On';
+							break;
+						case 'off' :
+							$popbounce_status = 'off';
+							$popbounce_title  = 'Off';
+							break;
+					}
+				}
+
+				echo '<div title="' . $popbounce_title . '" class="popbounce-status ' . $popbounce_status . '"></div>';
+		}
+	}
+
+	/**
+	 * Define the CSS for our custom column
+	 */
+	function post_columns_css()
+	{
 		$screen = get_current_screen();
 		if ( $screen->base == 'edit' ) {
-			wp_enqueue_style( 'wbounce-edit-page', plugins_url('../css/min/edit-page.css', __FILE__) );
+			wp_enqueue_style( 'popbounce-edit-page', plugins_url( '../css/min/edit-page.css', __FILE__ ) );
 		}
 	}
 
 }
 
-new Wbounce_Meta();
+new Popbounce_Meta();
---
+++ popbounce.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: popBounce
+Plugin URI: http://coldturkeygroup.com/
+Description: popBounce is a WordPress plugin build on ouiBounce to help increase conversion rates.
+Version: 1.0
+Author: Cold Turkey Group
+Author URI: http://coldturkeygroup.com/
+Text Domain: popbounce
+*/
+
+if ( ! defined( 'POPBOUNCE_OPTION_KEY' ) )
+	define( 'POPBOUNCE_OPTION_KEY', 'popbounce' );
+
+if ( ! defined( 'POPBOUNCE_VERSION_NUM' ) )
+	define( 'POPBOUNCE_VERSION_NUM', '1.0' );
+
+if ( ! defined( 'POPBOUNCE_VERSION_KEY' ) )
+	define( 'POPBOUNCE_VERSION_KEY', POPBOUNCE_OPTION_KEY . '_version' );
+
+// Store the plugin version for upgrades
+add_option( POPBOUNCE_VERSION_KEY, POPBOUNCE_VERSION_NUM );
+
+if ( ! defined( 'POPBOUNCE_PLUGIN_NAME' ) )
+	define( 'POPBOUNCE_PLUGIN_NAME', 'popBounce' );
+
+if ( ! defined( 'POPBOUNCE_TD' ) )
+	define( 'POPBOUNCE_TD', 'popbounce' );
+
+if ( ! defined( 'POPBOUNCE_FILE' ) )
+	define( 'POPBOUNCE_FILE', __FILE__ );
+
+if ( ! defined( 'POPBOUNCE_PATH' ) )
+	define( 'POPBOUNCE_PATH', plugin_dir_path( __FILE__ ) );
+
+require_once( POPBOUNCE_PATH . 'admin/class-register.php' );
+
+/**
+ * Class Popbounce_Init
+ *
+ * Basic initializer class for popBounce
+ */
+class Popbounce_Init {
+
+	/**
+	 * Load required functions based on admin or frontend
+	 */
+	function __construct()
+	{
+		if ( is_admin() ) {
+			add_action( 'plugins_loaded', [ $this, 'admin_init' ], 14 );
+		} else {
+			add_action( 'plugins_loaded', [ $this, 'frontend_init' ], 14 );
+		}
+	}
+
+	/**
+	 * Initialize required admin classes
+	 */
+	function admin_init()
+	{
+		require_once( POPBOUNCE_PATH . 'admin/class-admin-options.php' );
+		require_once( POPBOUNCE_PATH . 'admin/class-meta.php' );
+	}
+
+	/**
+	 * Initialize required frontend classes
+	 */
+	function frontend_init()
+	{
+		require_once( POPBOUNCE_PATH . 'frontend/class-frontend.php' );
+	}
+
+}
+
+new Popbounce_Init();
